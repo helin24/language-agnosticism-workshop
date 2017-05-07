ed6013a52b5db574fd6c53fcd8915b9d15eacc65
Helin Shiah <helin.shiah@gmail.com>
2017-05-07 14:04:25 -0700

    Add bad C code that seems to get a longest word

---
+++ easier-problem/C/part1.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <string.h>
+
+int main()
+{
+    char str[] = "Here is a long sentence!\n";
+    char * pointerToStringInArray;
+    printf(str);
+    
+    pointerToStringInArray = strtok(str, " ");
+    
+    char longestWordSoFar[1000] = "";
+    
+    while (pointerToStringInArray != NULL)
+    {
+        if (strlen(pointerToStringInArray) > strlen(longestWordSoFar)) 
+        {
+            strncpy(longestWordSoFar, pointerToStringInArray, 1000);
+        }
+
+        // printf("%i\n", strlen(pointerToStringInArray));
+        pointerToStringInArray = strtok(NULL, " ");
+    }
+    
+    printf("\n%s\n", longestWordSoFar);
+
+    return 0;
+}
+
